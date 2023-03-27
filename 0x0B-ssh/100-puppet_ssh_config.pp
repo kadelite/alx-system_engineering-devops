2bc5e8643aabd6beb0f674a272e45ff1b08e4931
kadelite <mechatronics.samson@gmail.com>
2023-03-27 20:52:04 +0100

    task ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# Puppet script to create ssh config file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
---
+++ 0x0B-ssh/README.md
@@ -0,0 +1,13 @@
+0x0B. SSH
+
+Tasks
+
+Use a private key
+
+Create an SSH key pair
+
+Client configuration file
+
+Let me in!
+
+Client configuration file (w/ Puppet)
